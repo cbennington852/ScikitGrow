c7ec72f7bd21ee7aaab3bf4869a71cbc6c6d3824
Charles Bennington <c.bennington852@gmail.com>
2025-11-10 16:54:09 -0800

    fix : install script

--- install_windows.sh
+++ install_windows.sh
@@ -1,6 +1,7 @@
 pacman -Suy
 pacman -S mingw-w64-ucrt-x86_64-gtk4 mingw-w64-ucrt-x86_64-python3 mingw-w64-ucrt-x86_64-python3-gobject
-sudo pacman -S python-pip
+pacman -S python
+pacman -S python-pip
 pip install -r requirements.txt
 glib-compile-resources reasources.xml --target=resources.gresource
 python -m venv .venv
