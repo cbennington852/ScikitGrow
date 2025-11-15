6e2450f7b21204a687e6a53718ef0556c469bfad
Charles Bennington <c.bennington852@gmail.com>
2025-11-15 15:31:39 -0800

    fix : win insatll

--- install_mac.sh
+++ install_mac.sh
@@ -2,4 +2,4 @@ brew install pygobject3 gtk4
 glib-compile-resources reasources.xml --target=resources.gresource
 python -m venv .venv
 source .venv/bin/activate
-pip install -r requirements.txt
+pip install scikit-learn matplotlib numpy 
