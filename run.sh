1052ff5d69f1cf1b4b66c006f3d6d916232cf3eb
Charles Bennington <c.bennington852@gmail.com>
2026-01-06 16:25:53 -0800

    feat : new example datasets

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 source ./myenv/bin/activate
-pyrcc5 -o image_resources.py images/resources.qrc
-python main.py example_datasets/test.csv
+pyrcc5 -o image_resources.py resources/resources.qrc
+#python main.py resources/example_datasets/test.csv
+python main.py
